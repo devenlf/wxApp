dc36d13450bb64c0dbe3e63800657b4d7fe9da5e
devenlf <1821415702@qq.com>
2018-08-02 18:15:20 +0800

    保存

---
+++ src/pages/logs/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+   
+  </div>
+</template>
+
+<script>
+
+export default {
+  components: {
+  },
+
+  data () {
+    return {
+      logs: []
+    }
+  },
+
+  created () {
+  }
+}
+</script>
+
+<style>
+.log-list {
+  display: flex;
+  flex-direction: column;
+  padding: 40rpx;
+}
+
+.log-item {
+  margin: 10rpx;
+}
+</style>
---
+++ static/iview/avatar/index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+Component({
+    externalClasses: ['i-class'],
+
+    properties: {
+        // circle || square
+        shape: {
+            type: String,
+            value: 'circle'
+        },
+        // small || large || default
+        size: {
+            type: String,
+            value: 'default'
+        },
+        src: {
+            type: String,
+            value: ''
+        }
+    }
+});
